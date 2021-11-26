1aadb51a380fcfe8a3157e2c191b99d0ed4b0a76
nptuYuJa <90607334+nptuYuJa@users.noreply.github.com>
2021-11-26 22:25:21 +0800

    Create Q5.c

---
+++ Q5.c
@@ -0,0 +1,19 @@
+//Q5
+#include<stdio.h>
+int main()
+{
+    //    length(x) 200 
+    //    witdth(y): 300 
+    //    so x range => -100 ~ 100
+    //    y range=> -150 ~ 150
+    int X, Y;
+    scanf("(%d,%d)", &X, &Y);
+    if(X<-100 || X>100 || Y<-150 || Y>150)
+    {
+        printf("Outside\n");
+    }
+    else
+    {
+        printf("Inside\n");
+    }
+}
